b41ef8ff32368b84ef82d59df745b71f4858cc01
Lee Jun Yong <junyongxlee@gmail.com>
2023-10-26 18:19:52 +0800

    Update PrintContent.java

--- android/src/main/java/com/example/bluetooth_print/PrintContent.java
+++ android/src/main/java/com/example/bluetooth_print/PrintContent.java
@@ -137,7 +137,7 @@ public class PrintContent {
             // 设置原点坐标
             tsc.addReference(0, 0);
             //设置浓度
-            tsc.addDensity(LabelCommand.DENSITY.DNESITY4);
+            tsc.addDensity(LabelCommand.DENSITY.DNESITY10);
             // 撕纸模式开启
             tsc.addTear(EscCommand.ENABLE.ON);
             // 清除打印缓冲区
